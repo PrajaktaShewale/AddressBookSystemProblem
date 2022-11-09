d7ab9df8fe84dff4abc744d06502b80833c207d8
PrajaktaShewale <shewalep94@gmail.com>
2022-11-09 16:31:07 +0530

    [Prajakta]Add:EditCreatedContact

--- AddressBookSystem/AddressBookMain.cs
+++ AddressBookSystem/AddressBookMain.cs
@@ -9,6 +9,7 @@ namespace AddressBookSystem
 {
     internal class AddressBookMain
     {
+        List<Contacts> addressBook = new List<Contacts>();
         Contacts contact = new Contacts();
         public void CreateContact()
         {
@@ -26,6 +27,43 @@ namespace AddressBookSystem
         {
             Console.WriteLine("contact Details" + "\n" + "First Name  " + contact.FirstName + "\n" + "Last Name  " + contact.LastName + "\n" + "Address  " + contact.Address + "\n" + "City  " + contact.
         City + "\n" + "State  " + contact.State + "\n" + "Zip  " + contact.Zip + "\n" + "Phn Number  " + contact.PhoneNumber + "\n" + "Email  " + contact.Email);
+        }
+        public void EditContact(string name)
+        {
+            CreateContact();
+            
+                if ((contact.FirstName.Equals(name)) || (contact.LastName.Equals(name)))
+                {
+                    Console.WriteLine("1.First Name 2.Last Name 3.Address 4.City 5.State 6.Zip 7.Phn Number 8.Email");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            contact.FirstName = Console.ReadLine();
+                            break;
+                        case 2:
+                            contact.LastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            contact.City = Console.ReadLine();
+                            break;
+                        case 4:
+                            contact.State = Console.ReadLine();
+                            break;
+                        case 5:
+                            contact.Zip = Convert.ToInt64(Console.ReadLine());
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            contact.PhoneNumber = Console.ReadLine();
+                            break;
+                        case 8:
+                            contact.Email = Console.ReadLine();
+                            break;
+                    }
+                    print();
+                }
         }
     }
 }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,19 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace AddressBookSystem
 {
     internal class Program
     {
-        static void Main(string[] args)
-{
-            AddressBookMain main = new AddressBookMain();
-            main.CreateContact();
+        public static void Main(string[] args)
+        {
+            bool option = true;
+            while (option)
+            {
+                AddressBookMain create = new AddressBookMain();
+                Console.WriteLine("1.Create  2.Edit  3.Exit");
+                        int select = Convert.ToInt32(Console.ReadLine());
+                switch (select)
+                {
+                    case 1:
+                        Console.WriteLine("First Name " + "\n" + "Last Name " + "\n" + "Address " + "\n" +
+                                        "City " + "\n" + "State " + "\n" + "Zip " + "\n" +
+                                        "Phone Num " + "\n" + "Email ");
+                        create.CreateContact();
+                        break;
+                    case 2:
+                        Console.WriteLine("First Name " + "\n" + "Last Name " + "\n" + "Address " + "\n" +
+                                        "City " + "\n" + "State " + "\n" + "Zip " + "\n" +
+                                        "Phone Num " + "\n" + "Email ");
+                        create.EditContact("Prajakta");
+                        break;
+                    default:
+                        option = false;
+                        break;
 
-    
+                }
+            }
         }
     }
 }
